9d1461d051b39625ea3843cbf0f0e9a74ba70409
DESKTOP-T9LEV0V\oslab <jinseok1006@naver.com>
2024-12-02 01:20:39 +0900

    파일 트랜스포트 추가

--- src/logger/winston.logger.ts
+++ src/logger/winston.logger.ts
@@ -5,33 +5,37 @@ import {
   WinstonModule,
 } from "nest-winston";
 
-// export const winstonLogger = winston.createLogger({
-//   level: 'silly',
-//   format: winston.format.combine(
-//     winston.format.timestamp(),
-//     nestWinstonModuleUtilities.format.nestLike('MyApp', {
-//       prettyPrint: true,
-//     }),
-//   ),
-//   transports: [
-//     new winston.transports.Console(),
-//     // 필요에 따라 파일로 로그를 저장하는 추가적인 트랜스포트를 설정할 수 있습니다.
-//     // new winston.transports.File({ filename: 'combined.log' }),
-//   ],
-// });
+
+// 에러 로그를 기록하는 파일 트랜스포트
+const errorFileTransport = new winston.transports.File({
+  filename: 'error.log',
+  level: 'error', // 'error' 레벨의 로그만 기록
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json() // JSON 형식으로 기록
+  ),
+});
+
+// 에러를 제외한 모든 로그를 기록하는 파일 트랜스포트
+const accessFileTransport = new winston.transports.File({
+  filename: 'access.log',
+  level: 'info', // 'info' 이상의 레벨의 로그만 기록
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+    // 'error' 레벨을 제외하는 필터 추가
+    winston.format((info) => {
+      return info.level !== 'error' ? info : false;
+    })()
+  ),
+});
 
 // Custom format similar to common web server log formats
 export const winstonLogger = WinstonModule.createLogger({
   level: process.env.NODE_ENV === "production" ? "info" : "silly",
   transports: [
-    // new winston.transports.File({
-    //   filename: "application.log", // 로그를 기록할 파일 이름
-    //   level: process.env.NODE_ENV === "production" ? "info" : "silly", // 환경에 따른 로그 레벨 설정
-    //   format: winston.format.combine(
-    //     winston.format.timestamp(), // 타임스탬프 추가
-    //     winston.format.json() // JSON 형식으로 포맷
-    //   ),
-    // }),
+    // errorFileTransport,
+    // accessFileTransport,
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.timestamp(),
