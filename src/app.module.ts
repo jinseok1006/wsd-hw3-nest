56c43592625d0668ee701a48287ab13c9b688c9c
DESKTOP-T9LEV0V\oslab <jinseok1006@naver.com>
2024-12-06 13:07:22 +0900

    캐시 인터셉터 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,15 @@ import { winstonOption } from "./logger/winston.logger";
 // import { LoggerModule } from './logger/logger.module';
 // import { RedisModule } from "./redis/redis.module";
 import { ConfigModule } from "@nestjs/config";
-import { TokenModule } from './token/token.module';
-import { JobsModule } from './jobs/jobs.module';
-import { BookmarksModule } from './bookmarks/bookmarks.module';
-import { ApplicationsModule } from './applications/applications.module';
-import { ReviewsModule } from './reviews/reviews.module';
-
+import { TokenModule } from "./token/token.module";
+import { JobsModule } from "./jobs/jobs.module";
+import { BookmarksModule } from "./bookmarks/bookmarks.module";
+import { ApplicationsModule } from "./applications/applications.module";
+import { ReviewsModule } from "./reviews/reviews.module";
+import { CacheModule } from "@nestjs/cache-manager";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import * as redisStore from "cache-manager-redis-store";
+import { GlobalCacheInterceptor } from "./common/cache/global-cache.interceptor";
 
 @Module({
   imports: [
@@ -28,8 +31,19 @@ import { ReviewsModule } from './reviews/reviews.module';
     BookmarksModule,
     ApplicationsModule,
     ReviewsModule,
+    CacheModule.register({
+      store: redisStore, // Redis 스토어 사용
+      host: "localhost", // Redis 서버 주소
+      port: 6379, // Redis 서버 포트
+      ttl: 600, // 기본 TTL 설정 (초 단위)
+    }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR, // 전역 인터셉터로 등록
+      useClass: GlobalCacheInterceptor,
+    },
+  ],
 })
 export class AppModule {
   // configure(consumer: MiddlewareConsumer) {
