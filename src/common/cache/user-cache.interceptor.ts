0d493afa3d82be2fcaa14a110578c95f5e3efbbd
DESKTOP-T9LEV0V\oslab <jinseok1006@naver.com>
2024-12-15 12:53:30 +0900

    빌드 관련 수정

--- src/common/cache/user-cache.interceptor.ts
+++ src/common/cache/user-cache.interceptor.ts
@@ -1,19 +1,27 @@
-import { Injectable, ExecutionContext, Inject } from "@nestjs/common";
+import {
+  Injectable,
+  ExecutionContext,
+  Inject,
+  LoggerService,
+} from "@nestjs/common";
 import { CACHE_MANAGER, CacheInterceptor } from "@nestjs/cache-manager";
 import { CacheKeyHelper } from "./cache-key-helper";
 import { Reflector } from "@nestjs/core";
 import { Cache } from "cache-manager";
+import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 
 @Injectable()
 export class UserCacheInterceptor extends CacheInterceptor {
   constructor(
     @Inject(CACHE_MANAGER) cacheManager: Cache,
-    reflector: Reflector
+    reflector: Reflector,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService
   ) {
     super(cacheManager, reflector);
   }
   trackBy(context: ExecutionContext): string | undefined {
     const request = context.switchToHttp().getRequest();
+    const handler = context.getHandler();
 
     // GET 요청만 캐싱
     if (request.method !== "GET") {
@@ -27,12 +35,20 @@ export class UserCacheInterceptor extends CacheInterceptor {
         request.url,
         request.user.sub
       );
-      console.log(`[CACHE DEBUG] Generated user-specific cache key: ${key}`);
+      // console.log(`[CACHE DEBUG] Generated user-specific cache key: ${key}`);
+      this.logger.debug({
+        message: `Generated user-specific cache key: ${handler.name}`,
+        context: UserCacheInterceptor.name,
+      });
       return key;
     }
 
     // 사용자 ID가 없는 요청은 캐싱하지 않음
-    console.log(`[CACHE DEBUG] No user ID found, skipping cache.`);
+    // console.log(`[CACHE DEBUG] No user ID found, skipping cache.`);
+    this.logger.debug({
+      message: `No user ID found, skipping cache: ${handler.name}`,
+      context: UserCacheInterceptor.name,
+    });
     return undefined;
   }
 }
